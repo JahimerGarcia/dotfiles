e8875562b0e7ac7f8bb533b0ee7f7572ad93c234
Jahimer Garcia <jahimergarcia@hotmail.com>
2024-02-25 22:25:23 +0000

    config: add dale and lobster

--- home.nix
+++ home.nix
@@ -20,16 +20,17 @@
   home.packages = [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
-    # pkgs.hello
+    pkgs.tmux
     pkgs.openssh
     pkgs.age
-    # pkgs.firefox
-    # pkgs.pavucontrol	
+    pkgs.unzip
+ #pkgs.pavucontrol	
+    pkgs.lobster
+    pkgs.dale
     pkgs.git    
     pkgs.neovim
     pkgs.go
     pkgs.rustup
-    pkgs.gcc
     (pkgs.nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
 # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
